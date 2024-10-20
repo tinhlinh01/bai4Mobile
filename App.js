8e1da3461a8472392297c1b5be3e8c045e665a36
tinhlinh01 <phungthilinhqucb@gmail.com>
2024-10-20 14:17:40 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function App() {
+const ColorChanger = () => {
+  const [bgColor, setBgColor] = useState("green"); // Initial background color
+
+  const changeBackgroundColor = (color) => {
+    setBgColor(color); // Change state when button is pressed
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "green" }]}
+        onPress={() => changeBackgroundColor("green")}
+      >
+        <Text style={styles.buttonText}>GREEN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "blue" }]}
+        onPress={() => changeBackgroundColor("blue")}
+      >
+        <Text style={styles.buttonText}>BLUE</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "brown" }]}
+        onPress={() => changeBackgroundColor("brown")}
+      >
+        <Text style={styles.buttonText}>BROWN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "yellow" }]}
+        onPress={() => changeBackgroundColor("yellow")}
+      >
+        <Text style={[styles.buttonText, { color: "black" }]}>YELLOW</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "red" }]}
+        onPress={() => changeBackgroundColor("red")}
+      >
+        <Text style={styles.buttonText}>RED</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "black" }]}
+        onPress={() => changeBackgroundColor("black")}
+      >
+        <Text style={[styles.buttonText, { color: "white" }]}>BLACK</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+export default ColorChanger;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  button: {
+    width: "80%",
+    padding: 20,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
